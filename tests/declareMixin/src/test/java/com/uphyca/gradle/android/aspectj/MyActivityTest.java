cda5f9db4bc1620945722e23505be3c1da317008
Sosuke Masui <esmasui@gmail.com>
2015-10-07 15:49:23 +0900

    Fix JVM test

--- tests/declareMixin/src/test/java/com/uphyca/gradle/android/aspectj/MyActivityTest.java
+++ tests/declareMixin/src/test/java/com/uphyca/gradle/android/aspectj/MyActivityTest.java
@@ -1,24 +1,21 @@
 package com.uphyca.gradle.android.aspectj;
 
-import android.widget.TextView;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
-import static org.assertj.android.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class MyActivityTest {
 
     @Test
-    public void greet() throws Exception {
+    public void feelingHappy() throws Exception {
         MyActivity underTest = Robolectric.setupActivity(MyActivity.class);
 
-        TextView greetingView = (TextView) underTest.findViewById(R.id.greeting);
-        assertThat(greetingView).hasText("Hello, aspect!");
+        assertThat(((MoodIndicator.Moody) underTest).getMood()).isEqualTo(Mood.HAPPY);
     }
 }
